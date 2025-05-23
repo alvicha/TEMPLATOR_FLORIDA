2196252c45f173bb42fc1dd2281909d343c681df
Alberto Villegas <alvich@floridauniversitaria.es>
2025-05-23 18:31:16 +0200

    Haciendo cambios

--- templator_frontend/src/components/FooterComponent.js
+++ templator_frontend/src/components/FooterComponent.js
@@ -3,7 +3,7 @@ const FooterComponent = () => {
     return (
         <footer className="row d-flex align-items-center">
             <div className="col-12 col-md-6 col-sm-6 text-md-left text-lg-left text-center description">
-                <p>Powered By Nethits</p>
+                <p>Powered By VillegasCompany</p>
             </div>
 
             <div className="col-12 col-md-6 col-sm-6 text-md-right text-lg-right text-center version">
